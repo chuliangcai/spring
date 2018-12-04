83880c974d2b4b8816ca484c65ac3fe30221cbf9
chuliangcai <790704125@qq.com>
2018-12-04 22:07:42 +0800

    Default Changelist

--- src/main/java/com/cyc/spring/controller/TestController.java
+++ src/main/java/com/cyc/spring/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.cyc.spring.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class TestController {
     @RequestMapping
     public String home() {
         return "hello world";
+    }
+
+    public static void main(String[] args) throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(objectMapper.writeValueAsString(new Student()));
     }
 }

81610ff2956739b404fc5f4c126c34e7f1dc89df
chu.liangcai <790704125@qq.com>
2019-03-16 10:40:00 +0800

    test

--- src/main/java/com/cyc/spring/Application.java
+++ src/main/java/com/cyc/spring/Application.java
@@ -1,14 +1,18 @@
 package com.cyc.spring;
 
+import com.cyc.spring.proecessors.MyTypeFilter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 import java.io.File;
 import java.util.*;
 
 @SpringBootApplication
+@ComponentScan(includeFilters = {@ComponentScan.Filter(type = FilterType.CUSTOM,classes = MyTypeFilter.class)})
 public class Application {
 
     public static void main(String[] args) throws Exception {
